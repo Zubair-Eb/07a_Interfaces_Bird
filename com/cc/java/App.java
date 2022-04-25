f51ebe8bc697faaec7a2606009d0f7aefcac0fc0
Zubair <zubair.yousaf-eberl@gmx.de>
2022-04-25 11:47:28 +0200

    Implements

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -6,6 +6,8 @@ public class App {
 
 
         Bird bird = new Bird();
+        output(bird.fly());
+        output(bird.hasFeathers());
 
     
     }
--- com/cc/java/Bird.java
+++ com/cc/java/Bird.java
@@ -1,9 +1,17 @@
 package com.cc.java;
 
-public class Bird{
+public class Bird implements Flyable, IFeathers {
+
+    @Override
+    public String fly() {
+        return "I'm a bird, I can also fly.";
+    }
+
+    @Override
+    public String hasFeathers() {
+        return "I'm a bird, I have some feathers.";
+    }
 
 
-
-    
-    
+  
 }
