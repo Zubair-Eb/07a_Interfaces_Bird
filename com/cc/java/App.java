ce748dacf543eca060353fdffed25a71776e996f
Peter Schmidt <schmidt.multimedia@gmail.com>
2022-04-25 10:42:34 +0200

    Add files via upload

---
+++ com/cc/java/App.java
@@ -0,0 +1,18 @@
+package com.cc.java;
+
+public class App {
+    
+    public static void main(String[] args) {
+
+
+        Bird bird = new Bird();
+
+    
+    }
+
+    private static void output(String outStr) {
+        System.out.println(outStr);
+    }
+
+
+}
